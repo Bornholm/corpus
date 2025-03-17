58ee893041a822964050ba712242bb980f5fef36
William Petit <wpetit@cadoles.com>
2025-03-17 19:40:48 +0100

    feat: push latest docker tag on release

--- misc/docker/docker.mk
+++ misc/docker/docker.mk
@@ -11,4 +11,6 @@ docker-run:
 	docker run -it --rm  --name corpus --env-file .env $(DOCKER_IMAGE_NAME):$(DOCKER_IMAGE_TAG)
 
 docker-release: docker-image
+	docker tag $(DOCKER_IMAGE_NAME):$(DOCKER_IMAGE_TAG) $(DOCKER_IMAGE_NAME):latest
 	docker push $(DOCKER_IMAGE_NAME):$(DOCKER_IMAGE_TAG)
+	docker push $(DOCKER_IMAGE_NAME):latest
