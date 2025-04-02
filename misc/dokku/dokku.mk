fe861dc054aef220375250c6567cabd0d710c1e4
William Petit <wpetit@cadoles.com>
2025-04-02 21:50:17 +0200

    chore: use atomic push to dokku

--- misc/dokku/dokku.mk
+++ misc/dokku/dokku.mk
@@ -2,4 +2,4 @@ DOKKU_APP ?= corpus
 DOKKU_DEPLOY_URL ?= dokku@dev.lookingfora.name
 
 dokku-deploy:
-	git push $(DOKKU_DEPLOY_URL):$(DOKKU_APP) $(shell git rev-parse HEAD):refs/heads/master --force
+	git push --atomic $(DOKKU_DEPLOY_URL):$(DOKKU_APP) $(shell git rev-parse HEAD):refs/heads/master --force
