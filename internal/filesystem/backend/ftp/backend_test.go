7fc6ef172fb8d6530865b0424236310e0618a90c
William Petit <wpetit@cadoles.com>
2025-04-27 22:22:34 +0200

    chore: disable ftp test in ci

--- internal/filesystem/backend/ftp/backend_test.go
+++ internal/filesystem/backend/ftp/backend_test.go
@@ -3,6 +3,7 @@ package ftp
 import (
 	"context"
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/bornholm/corpus/internal/filesystem/testsuite"
@@ -14,6 +15,11 @@ import (
 )
 
 func TestWatch(t *testing.T) {
+	if os.Getenv("CI") == "true" {
+		t.Skip("Test skipped in CI environment because of FTP incompatibility with containerized environment")
+		return
+	}
+
 	ctx := context.Background()
 	req := testcontainers.ContainerRequest{
 		Image: "docker.io/garethflowers/ftp-server",
