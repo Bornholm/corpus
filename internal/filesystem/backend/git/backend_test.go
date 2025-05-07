5c8c96cdd5f97d141939c88490262186e322d01e
William Petit <wpetit@cadoles.com>
2025-05-07 12:06:55 +0200

    chore: disable git backend test in ci environment

--- internal/filesystem/backend/git/backend_test.go
+++ internal/filesystem/backend/git/backend_test.go
@@ -2,12 +2,18 @@ package git
 
 import (
 	"log/slog"
+	"os"
 	"testing"
 
 	"github.com/bornholm/corpus/internal/filesystem/testsuite"
 )
 
 func TestWatch(t *testing.T) {
+	if os.Getenv("CI") == "true" {
+		t.Skip("Test skipped in CI environment because of non contained test")
+		return
+	}
+
 	slog.SetLogLoggerLevel(slog.LevelDebug)
 
 	testsuite.TestWatch(t, "git://github.com/Bornholm/corpus.git?gitPullInterval=2s")
